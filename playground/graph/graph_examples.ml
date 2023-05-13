2730adeb273067d1eb6a882be53c2797132e0c21
tjazerzen <erzen.tjaz@gmail.com>
2023-05-13 15:58:40 +0200

    Replace Printf.printf with print_endline where relevant

--- playground/graph/graph_examples.ml
+++ playground/graph/graph_examples.ml
@@ -1,13 +1,12 @@
 open Graph
 
 (* #use "graph_new_examples.ml";; *)
-let () = Printf.printf "Creating nodes with values 4, 5, 6...\n"
+let () = print_endline "Creating nodes with values 4, 5, 6..."
 let node1 = Node.create 4
 let node2 = Node.create 5
 let node3 = Node.create 6
 
-
-let () = Printf.printf "Creating graph with nodes: {node1, node2, node3} and creating edges node1-node2 and node2-node3...\n"
+let () = print_endline "Creating graph with nodes: {node1, node2, node3} and creating edges node1-node2 and node2-node3..."
 let my_graph = 
   Graph.empty true
   |> Graph.add_node node1 
@@ -16,29 +15,28 @@ let my_graph =
   |> Graph.add_edge node1 node2
   |> Graph.add_edge node2 node3
 
-
-let () = Printf.printf "Printing graph to string...\n"
+let () = print_endline "Printing graph to string..."
 let () = Printf.printf "Graph: %s\n" (Graph.to_string my_graph)
 
-let () = Printf.printf "Removing node1 from graph...\n"
+let () = print_endline "Removing node1 from graph..."
 let my_graph = Graph.remove_node node1 my_graph
 
-let () = Printf.printf "Printing graph to string once again...\n"
+let () = print_endline "Printing graph to string once again..."
 let () = Printf.printf "Graph: %s\n" (Graph.to_string my_graph)
 
-let () = Printf.printf "Removing edge node2-node3 from graph...\n"
+let () = print_endline "Removing edge node2-node3 from graph..."
 let my_graph = Graph.remove_edge node2 node3 my_graph
 let () = Printf.printf "Graph: %s\n" (Graph.to_string my_graph)
 
 let nodes = Graph.nodes my_graph
-let () = Printf.printf "Printing nodes of graph...\n"
+let () = print_endline "Printing nodes of graph..."
 let () = Printf.printf "Nodes: %s\n" (nodes |> List.map Node.to_string |> String.concat ", ")
 
-let () = Printf.printf "Connecting node2 and node3 with edge...\n"
+let () = print_endline "Connecting node2 and node3 with edge..."
 let my_graph = Graph.add_edge node2 node3 my_graph
 
+let () = print_endline "Printing edges of graph..."
 
-let () = Printf.printf "Printing edges of graph...\n"
 let edges = Graph.edges my_graph
 let () = Printf.printf "Edges: %s\n" 
   (edges 
