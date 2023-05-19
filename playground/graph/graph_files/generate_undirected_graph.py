748a3f8bac0d402edd25c38e0e26100f38bd3fd1
tjazerzen <erzen.tjaz@gmail.com>
2023-05-19 15:45:45 +0200

    Fix graph generation

--- playground/graph/graph_files/generate_undirected_graph.py
+++ playground/graph/graph_files/generate_undirected_graph.py
@@ -2,24 +2,27 @@ import random
 
 print_again = True
 
-def generate_graph(num_of_nodes, max_num_of_edges, file_name):
+def generate_graph(num_of_nodes, num_of_edges, file_name):
     existing_edges = set()
     with open(file_name, 'w') as f:
         # Generate nodes
-        for i in range(1, num_of_nodes + 1):
+        for i in range(0, num_of_nodes):
             f.write(f'{i}:{random.randint(1, num_of_nodes*4)}\n')
 
         f.write('\n')
 
         # Generate edges
-        for _ in range(max_num_of_edges):
-            start_node = random.randint(1, num_of_nodes)
-            end_node = random.randint(1, num_of_nodes)
+        current_edges_added = 0
+        while current_edges_added != num_of_edges:
+            start_node = random.randint(0, num_of_nodes-1)
+            end_node = random.randint(0, num_of_nodes-1)
             if start_node == end_node:
                 continue
             if (start_node, end_node) in existing_edges or (end_node, start_node) in existing_edges:
                 continue
+            existing_edges.add((start_node, end_node))
+            current_edges_added += 1
             f.write(f'{start_node}-{end_node}\n')
 
 if print_again:
-    generate_graph(num_of_nodes=100, max_num_of_edges=2000, file_name='undirected_graph_large.txt')
+    generate_graph(num_of_nodes=5, num_of_edges=10, file_name='undirected_small_graph.txt')
