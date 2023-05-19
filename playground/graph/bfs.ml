bac8aa6a3459c3f267e8d394120f8181b8d45903
tjazerzen <erzen.tjaz@gmail.com>
2023-05-19 15:46:08 +0200

    Fix bfs implementation

--- playground/graph/bfs.ml
+++ playground/graph/bfs.ml
@@ -26,31 +26,30 @@ end = struct
 end
 
 module Bfs : sig
-  val bfs : Graph.t -> Node.t -> unit
-  val parallel_bfs1 : Graph.t -> Node.t -> int -> int NodeMap.t
-  val parallel_bfs2 : Graph.t -> Node.t -> int -> int NodeMap.t
-  val parallel_bfs3 : Graph.t -> Node.t -> int -> unit
+  val bfs : Graph.t -> Node.t -> int NodeMap.t
+  val parallel_bfs : Graph.t -> Node.t -> int -> int NodeMap.t
 end = struct
   let bfs graph start_node =
-    (*Rekurzivna sekvenčna implementacija algoritma BFS*)
     let visited = ref NodeSet.empty in
+    let level = ref NodeMap.empty in
     let queue = Queue.create_empty_queue () in
-
-    (* Mark the start node as visited and enqueue it *)
+  
     visited := NodeSet.add start_node !visited;
+    level := NodeMap.add start_node 0 !level;
     let queue = Queue.enqueue start_node queue in
-
+  
     let rec loop q =
       if not (Queue.is_empty q) then
         match Queue.dequeue q with
         | Some (node, next_q) ->
-            Printf.printf "Visiting node %s\n" (Node.to_string node);
             let neighbours = Graph.neighbours node graph in
+            let parent_level = NodeMap.find node !level in
             let new_q =
               List.fold_left
                 (fun acc_q neighbour ->
                   if not (NodeSet.mem neighbour !visited) then (
                     visited := NodeSet.add neighbour !visited;
+                    level := NodeMap.add neighbour (parent_level + 1) !level;
                     Queue.enqueue neighbour acc_q)
                   else acc_q)
                 next_q neighbours
@@ -58,15 +57,11 @@ end = struct
             loop new_q
         | None -> ()
     in
-    loop queue
+    loop queue;
+    !level
+  
 
-    let parallel_bfs1 graph start_node num_domains =
-      (*
-      Rekurzivna paralelna implementacija algoritma BFS.
-      Uporaba Domainslib knjižnice.
-      Za uporabo paralelizacije uporabljam Domainslib.Task.parallel_for.
-      Napaka, ki jo dobim: Fatal error: exception Stdlib.Effect.Unhandled(Domainslib__Task.Wait(_, _))
-      *)
+    let parallel_bfs graph start_node num_domains =
       let visited = ref NodeSet.empty in
       let level = ref (NodeMap.empty) in
       let queue = Queue.create_empty_queue () in
@@ -99,111 +94,10 @@ end = struct
               bfs_inner ()
           | None -> failwith "Unexpected empty queue in BFS" 
       in
-      bfs_inner ();
+      T.run pool (fun () -> bfs_inner ());
       T.teardown_pool pool;
     
       !level  
-
-    let parallel_bfs2 graph start_node num_domains =
-      (*
-      Rekurzivna paralelna implementacija algoritma BFS.
-      V primerjavi z parallel_bfs1, dodatna uporaba Domainslib.Chan (channel) knjižnice.
-      Za razliko od parallel_bfs1, kjer uporabljam Domainslib.Task.parallel_for, 
-      tukaj uporabljam direktno definicijo taskov z Domainslib.Task.async, na katere počakam z Domainslib.Task.await.
-      Napaka: Fatal error: exception Stdlib.Effect.Unhandled(Domainslib__Task.Wait(_, _))
-      *)
-      let visited = ref NodeSet.empty in
-      let level = ref (NodeMap.empty) in
-      let channel = Domainslib.Chan.make_unbounded () in
-  
-      let pool = T.setup_pool ~num_domains:(num_domains - 1) () in
-  
-      Domainslib.Chan.send channel (Some start_node);
-      visited := NodeSet.add start_node !visited;
-      level := NodeMap.add start_node 0 !level;
-  
-      let rec bfs_inner () =
-        match Domainslib.Chan.recv channel with
-        | Some node ->
-            let neighbours = Graph.neighbours node graph in
-            let tasks = List.mapi 
-              (fun _ neighbour -> T.async pool (fun _ -> 
-                if not (NodeSet.mem neighbour !visited) then begin
-                  visited := NodeSet.add neighbour !visited;
-                  let parent_level =
-                    match NodeMap.find_opt node !level with
-                    | Some l -> l
-                    | None -> failwith "Unexpected missing parent level in BFS"
-                  in
-                  level := NodeMap.add neighbour (parent_level + 1) !level;
-                  Domainslib.Chan.send channel (Some neighbour)
-                end))
-              neighbours in
-            List.iter (T.await pool) tasks;
-            bfs_inner ()
-        | None -> ()
-      in
-      bfs_inner ();
-      T.teardown_pool pool;
-  
-      !level
-    
-      let parallel_bfs3 graph start_node num_domains =
-        (*
-        Iterativna implementacija paralelnega algoritma BFS.
-        Uporabljam Domainslib.Task.parallel_for.
-        Dodatno tu uporabim modul Mutex, ki je zadolžen za to, da med 
-        sočasnim izvajanjem ne pride do sočasnega spreminjanja podatkovnih struktur.
-        Napaka: Fatal error: exception Stdlib.Effect.Unhandled(Domainslib__Task.Wait(_, _))
-        *)
-        let visited = ref NodeSet.empty in
-        let queue = ref (Queue.create_empty_queue ()) in
-        let queue_mutex = Mutex.create () in
-        let visited_mutex = Mutex.create () in
-    
-        (* Mark the start node as visited and enqueue it *)
-        Mutex.lock visited_mutex;
-        visited := NodeSet.add start_node !visited;
-        Mutex.unlock visited_mutex;
-    
-        Mutex.lock queue_mutex;
-        queue := Queue.enqueue start_node !queue;
-        Mutex.unlock queue_mutex;
-    
-        let pool = T.setup_pool ~num_domains:(num_domains - 1) () in
-    
-        while not (Queue.is_empty !queue) do
-          (* Dequeue node *)
-          Mutex.lock queue_mutex;
-          let (node, queue') = Option.get (Queue.dequeue !queue) in
-          queue := queue';
-          Mutex.unlock queue_mutex;
-          
-          Printf.printf "Visiting node %s\n" (Node.to_string node);
-    
-          let neighbours = Graph.neighbours node graph in
-    
-          (* Visit each neighbour in parallel *)
-          T.parallel_for pool ~start:0 ~finish:(List.length neighbours - 1)
-            ~body:(fun i ->
-              let neighbour = List.nth neighbours i in
-    
-              Mutex.lock visited_mutex;
-              if not (NodeSet.mem neighbour !visited) then begin
-                visited := NodeSet.add neighbour !visited;
-                Mutex.unlock visited_mutex;
-    
-                Mutex.lock queue_mutex;
-                queue := Queue.enqueue neighbour !queue;
-                Mutex.unlock queue_mutex;
-              end else
-                Mutex.unlock visited_mutex;
-            );
-        done;
-    
-        T.teardown_pool pool;
-    
-    
 end
 
 
