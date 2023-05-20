d4da72ab0f7fb5b8593ea6845f315c99c4ff49f4
tjazerzen <erzen.tjaz@gmail.com>
2023-05-20 09:27:27 +0200

    Rename methods in bfs.ml to be more informative

--- playground/graph/bfs_examples.ml
+++ playground/graph/bfs_examples.ml
@@ -7,25 +7,20 @@ let print_int_nodemap nodemap =
       Printf.printf "Node: %s - distance from start node: %s\n" (Node.to_string key) (string_of_int value)
     ) nodemap
 
-let graph_file_name = "graph_files/undirected_graph_large.txt"
+let graph_file_name = "graph_files/undirected_small_graph.txt"
+let start_node_id = 2
 
 let () = Printf.printf "Reading graph from file %s...\n" graph_file_name
 
-let new_graph = Graph.graph_from_txt graph_file_name
-let node1 = Option.get (Graph.find_node_by_id 0 new_graph)
+let new_graph = graph_file_name |> Graph.graph_from_txt 
+let node1 = Option.get (Graph.find_node_by_id start_node_id new_graph)
 
 let () = Printf.printf "Running SEQUENTIAL BFS on graph, stored in %s, \
-    starting with node with ID 0...\n" graph_file_name
+    starting with node with ID %d...\n" graph_file_name start_node_id
 
-let levels_nodemap_sequential = Bfs.bfs new_graph node1
-let () = print_int_nodemap levels_nodemap_sequential
-
+let () = (Bfs.bfs_sequential new_graph node1) |> print_int_nodemap
 
 let () = Printf.printf "Running PARALLEL BFS on graph, stored in %s, \
-    starting with node with ID 0...\n" graph_file_name
+    starting with node with ID %d...\n" graph_file_name start_node_id
 
-
-let levels_nodemap_parallel = Bfs.parallel_bfs new_graph node1 4
-
-let () = print_int_nodemap levels_nodemap_parallel
-
+let () = (Bfs.bfs_parallel new_graph node1 10) |> print_int_nodemap
