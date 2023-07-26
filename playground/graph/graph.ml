49af510706d7e5f30b32754ca4a4a7cfef94afc3
tjazerzen <erzen.tjaz@gmail.com>
2023-07-26 14:41:12 +0200

    Reformat graph*.ml files

--- playground/graph/graph.ml
+++ playground/graph/graph.ml
@@ -4,6 +4,7 @@ module Node : sig
 
   val create : elt -> t
   val value : t -> elt
+
   (* compare x y returns 0 if x is equal to y, a negative integer if x is less than y, and a positive integer if x is greater than y *)
   val compare_ids : t -> t -> int
   val compare : t -> t -> int
@@ -23,7 +24,6 @@ end = struct
 
   let value node = node.value
   let compare_ids node1 node2 = Stdlib.compare node1.id node2.id
-
   let compare node1 node2 = Stdlib.compare node1.value node2.value
 
   let to_string node =
@@ -136,7 +136,8 @@ end = struct
     try Some (List.find (fun node -> Node.id node = id) nodes)
     with Not_found -> None
 
-  let create_new_graph ~(num_nodes : int) ~(num_edges : int) ~(directed : bool) : t =
+  let create_new_graph ~(num_nodes : int) ~(num_edges : int) ~(directed : bool)
+      : t =
     let graph = empty ~directed in
     let nodes = List.init num_nodes (fun i -> Node.create i) in
     let graph =
@@ -156,124 +157,130 @@ end = struct
     add_edges graph num_edges
 end
 
-
 module WeightedGraph : sig
   type elt = int
   type t
 
-(** [empty ~directed] returns an empty graph with the specified directionality. *)
   val empty : directed:bool -> t
+  (** [empty ~directed] returns an empty graph with the specified directionality. *)
 
-(** [add_node node graph] returns a new graph with [node] added to it. *)
   val add_node : Node.t -> t -> t
-  
-(** [remove_node node graph] returns a new graph with [node] removed from it. *)
+  (** [add_node node graph] returns a new graph with [node] added to it. *)
+
   val remove_node : Node.t -> t -> t
-  
-(** [add_edge node1 node2 weight graph] returns a new graph with an edge between [node1] and [node2] added to it, with the specified [weight]. *)
+  (** [remove_node node graph] returns a new graph with [node] removed from it. *)
+
   val add_edge : Node.t -> Node.t -> float -> t -> t
-  
-(** [remove_edge node1 node2 graph] returns a new graph with the edge between [node1] and [node2] removed from it. *)
+  (** [add_edge node1 node2 weight graph] returns a new graph with an edge between [node1] and [node2] added to it, with the specified [weight]. *)
+
   val remove_edge : Node.t -> Node.t -> t -> t
-  
-(** [nodes graph] returns a list of nodes in the graph. *)
+  (** [remove_edge node1 node2 graph] returns a new graph with the edge between [node1] and [node2] removed from it. *)
+
   val nodes : t -> Node.t list
-  
-(** [to_string graph] returns a string representation of the graph [graph], including a list of its nodes and edges. *)
+  (** [nodes graph] returns a list of nodes in the graph. *)
+
   val to_string : t -> string
-  
-(** [neighbours node graph] returns a list of nodes that are adjacent to [node] in the graph [graph]. *)
+  (** [to_string graph] returns a string representation of the graph [graph], including a list of its nodes and edges. *)
+
   val neighbours : Node.t -> t -> Node.t list
-  
-(** [find_node_by_id id graph] returns the node in [graph] with the specified [id], if it exists. Otherwise, returns [None]. *)
+  (** [neighbours node graph] returns a list of nodes that are adjacent to [node] in the graph [graph]. *)
+
   val find_node_by_id : int -> t -> Node.t option
-  
-(** [create_new_graph ~num_nodes ~num_edges ~directed] returns a new graph with [num_nodes] nodes and [num_edges] edges, with the specified directionality. *)
+  (** [find_node_by_id id graph] returns the node in [graph] with the specified [id], if it exists. Otherwise, returns [None]. *)
+
   val create_new_graph : num_nodes:int -> num_edges:int -> directed:bool -> t
+  (** [create_new_graph ~num_nodes ~num_edges ~directed] returns a new graph with [num_nodes] nodes and [num_edges] edges, with the specified directionality. *)
 
-(** [edges graph] returns a map of maps representing the edges in the graph [graph]. The outer map maps each node in the graph to a map of its neighbours, where the inner map maps each neighbour to the weight of the edge connecting it to the node. *)
   val edges : t -> float NodeMap.t NodeMap.t
-
+  (** [edges graph] returns a map of maps representing the edges in the graph [graph]. The outer map maps each node in the graph to a map of its neighbours, where the inner map maps each neighbour to the weight of the edge connecting it to the node. *)
 end = struct
-  
   type elt = int
-  type t = {
-    edges : float NodeMap.t NodeMap.t;
-    directed : bool;
-  }
+  type t = { edges : float NodeMap.t NodeMap.t; directed : bool }
 
   let empty ~directed = { edges = NodeMap.empty; directed }
-
   let edges graph = graph.edges
 
-  let add_node (node: Node.t) (graph: t) : t =
+  let add_node (node : Node.t) (graph : t) : t =
     let edges = NodeMap.add node NodeMap.empty graph.edges in
     { graph with edges }
 
-  let remove_node (node: Node.t) (graph: t) : t =
+  let remove_node (node : Node.t) (graph : t) : t =
     let edges = NodeMap.remove node graph.edges in
     let edges = NodeMap.map (NodeMap.remove node) edges in
     { graph with edges }
 
-  let add_edge (src: Node.t) (dst: Node.t) (weight: float) (graph: t) : t =
-    let edges = NodeMap.update src (function
-      | Some map -> Some (NodeMap.add dst weight map)
-      | None -> Some (NodeMap.singleton dst weight)
-    ) graph.edges in
+  let add_edge (src : Node.t) (dst : Node.t) (weight : float) (graph : t) : t =
+    let edges =
+      NodeMap.update src
+        (function
+          | Some map -> Some (NodeMap.add dst weight map)
+          | None -> Some (NodeMap.singleton dst weight))
+        graph.edges
+    in
     let edges =
       if graph.directed then edges
-      else NodeMap.update dst (function
-        | Some map -> Some (NodeMap.add src weight map)
-        | None -> Some (NodeMap.singleton src weight)
-      ) edges
+      else
+        NodeMap.update dst
+          (function
+            | Some map -> Some (NodeMap.add src weight map)
+            | None -> Some (NodeMap.singleton src weight))
+          edges
     in
     { graph with edges }
 
-  let remove_edge (src: Node.t) (dst: Node.t) (graph: t) : t =
-    let edges = NodeMap.update src (function
-      | Some map -> Some (NodeMap.remove dst map)
-      | None -> assert false (* nodes should already exist in the graph *)
-    ) graph.edges in
+  let remove_edge (src : Node.t) (dst : Node.t) (graph : t) : t =
+    let edges =
+      NodeMap.update src
+        (function
+          | Some map -> Some (NodeMap.remove dst map)
+          | None -> assert false (* nodes should already exist in the graph *))
+        graph.edges
+    in
     let edges =
       if graph.directed then edges
-      else NodeMap.update dst (function
-        | Some map -> Some (NodeMap.remove src map)
-        | None -> assert false (* nodes should already exist in the graph *)
-      ) edges
+      else
+        NodeMap.update dst
+          (function
+            | Some map -> Some (NodeMap.remove src map)
+            | None -> assert false (* nodes should already exist in the graph *))
+          edges
     in
     { graph with edges }
 
-  let nodes (graph: t) : Node.t list = NodeMap.bindings graph.edges |> List.map fst
+  let nodes (graph : t) : Node.t list =
+    NodeMap.bindings graph.edges |> List.map fst
 
   let to_string (graph : t) : string =
     let nodes_str =
-      nodes graph |> List.map (fun node -> Node.to_string node)
+      nodes graph
+      |> List.map (fun node -> Node.to_string node)
       |> String.concat ", "
     in
-    let edges_aux (graph : t) : (Node.t * Node.t * float) list = 
+    let edges_aux (graph : t) : (Node.t * Node.t * float) list =
       graph.edges |> NodeMap.bindings
       |> List.map (fun (node1, nodes) ->
              NodeMap.bindings nodes
-             |> List.map (fun (node2, weight) -> (node1, node2, weight))
-      )
+             |> List.map (fun (node2, weight) -> (node1, node2, weight)))
       |> List.flatten
-      in
+    in
     let edges_str =
-      edges_aux graph |> List.map (fun (src, dst, weight) ->
-        Printf.sprintf "(%s -> %s : %f)" (Node.to_string src) (Node.to_string dst) weight
-      ) |> String.concat ", "
+      edges_aux graph
+      |> List.map (fun (src, dst, weight) ->
+             Printf.sprintf "(%s -> %s : %f)" (Node.to_string src)
+               (Node.to_string dst) weight)
+      |> String.concat ", "
     in
     Printf.sprintf "{ nodes: [%s]; edges: [%s] }" nodes_str edges_str
 
-  let neighbours (node: Node.t) (graph: t) : Node.t list =
-    try
-      NodeMap.find node graph.edges |> NodeMap.bindings |> List.map fst
+  let neighbours (node : Node.t) (graph : t) : Node.t list =
+    try NodeMap.find node graph.edges |> NodeMap.bindings |> List.map fst
     with Not_found -> []
 
-  let find_node_by_id (id: int) (graph: t) : Node.t option =
+  let find_node_by_id (id : int) (graph : t) : Node.t option =
     nodes graph |> List.find_opt (fun node -> Node.id node = id)
 
-  let create_new_graph ~(num_nodes : int) ~(num_edges : int) ~(directed : bool) : t =
+  let create_new_graph ~(num_nodes : int) ~(num_edges : int) ~(directed : bool)
+      : t =
     let graph = empty ~directed in
     let nodes = List.init num_nodes (fun i -> Node.create i) in
     let graph =
@@ -291,5 +298,4 @@ end = struct
     in
     let () = Node.reset_ids () in
     add_edges graph num_edges
-
 end
