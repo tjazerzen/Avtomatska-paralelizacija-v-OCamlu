19dd5e9b9f1abaaaba37ff5ba09f3a452552a8c9
tjazerzen <erzen.tjaz@gmail.com>
2023-08-01 14:15:25 +0200

    Module Queue is not used

--- playground/graph/bfs.ml
+++ playground/graph/bfs.ml
@@ -2,40 +2,6 @@ open Graph
 open Parany
 module T = Domainslib.Task
 
-(** [Queue] is a module that implements a queue data structure using two stacks.
-    The first stack is used for enqueueing elements, while the second stack 
-    is used for dequeueing elements. *)
-module Queue : sig
-  type 'a t
-
-  val create_empty_queue : unit -> 'a t
-  (** [create_empty_queue ()] creates an empty queue. *)
-
-  val is_empty : 'a t -> bool
-  (** [is_empty q] returns [true] if the queue [q] is empty, [false] otherwise. *)
-
-  val enqueue : 'a -> 'a t -> 'a t
-  (** [enqueue x q] adds element [x] to the queue [q]. *)
-
-  val dequeue : 'a t -> ('a * 'a t) option
-  (** [dequeue q] removes and returns the first element of the queue [q].
-    If the queue is empty, returns [None]. *)
-end = struct
-  type 'a t = { enqueue_stack : 'a list; dequeue_stack : 'a list }
-
-  let create_empty_queue () = { enqueue_stack = []; dequeue_stack = [] }
-  let is_empty q = q.enqueue_stack = [] && q.dequeue_stack = []
-  let enqueue x q = { q with enqueue_stack = x :: q.enqueue_stack }
-
-  let dequeue q =
-    match q.dequeue_stack with
-    | hd :: tl -> Some (hd, { q with dequeue_stack = tl })
-    | [] -> (
-        match List.rev q.enqueue_stack with
-        | [] -> None
-        | hd :: tl -> Some (hd, { enqueue_stack = []; dequeue_stack = tl }))
-end
-
 (** This module type will implement the breadth-first search algorithm on a graph. 
     It provides two functions, [parallel] and [sequential], that take a graph 
     and a starting node and return a list of sets of nodes, where each set 
